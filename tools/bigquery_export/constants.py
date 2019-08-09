8f5cbc38d2edca04b4fa44daa62d9d7efc052348
Monobina Saha <monobina@google.com>
2019-08-08 17:31:41 -0700

    bigquery delta export to GCS

---
+++ tools/bigquery_export/constants.py
@@ -0,0 +1,10 @@
+class Constants:
+    KEY_FILE = 'key_file'
+    BUCKET_NAME = 'bucket_name'
+    DEST_PROJECT_ID = "dest_project_id"
+    DEST_DATASET_ID = "dest_dataset_id"
+    DEST_TABLE_ID = "dest_table_id"
+    SOURCE_TABLE_ID = "source_table_id"
+    SOURCE_DATASET_ID = "source_dataset_id"
+    SOURCE_PROJECT_ID = "source_project_id"
+    DUMP_FILE_NAME = "dump_file_name"
